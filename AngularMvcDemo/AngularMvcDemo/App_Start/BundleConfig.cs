8b1d1c42c5c52954cbb5b14e96e53da63e67f6e1
Marius Schulz <marius.schulz@me.com>
2014-03-24 09:14:49 +0100

    Created project + removed a lot of unnecessary NuGet packages

---
+++ AngularMvcDemo/AngularMvcDemo/App_Start/BundleConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Optimization;
+
+namespace AngularMvcDemo
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}

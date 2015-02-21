f4b225d7d9e792b8a60fdc692d9255ba5ba9790d
Marius Schulz <marius.schulz@me.com>
2015-02-21 23:21:35 +0100

    Adds MVC project

---
+++ Bundling and Minifying Angular Scripts with MVC/AngularMvcBundlingMinification/AngularMvcBundlingMinification/App_Start/RouteConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AngularMvcBundlingMinification
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("Default", "{controller}/{action}", new { controller = "Home", action = "Index" });
+        }
+    }
+}
---
+++ Bundling and Minifying Angular Scripts with MVC/AngularMvcBundlingMinification/AngularMvcBundlingMinification/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularMvcBundlingMinification.MvcApplication" Language="C#" %>

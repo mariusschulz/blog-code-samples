4d2920c1c82dc40617e67b23cd69720bd56cdf74
Marius Schulz <marius.schulz@me.com>
2015-02-22 13:40:36 +0100

    Introduces a local constant for the virtual bundle path

--- Bundling and Minifying Angular Scripts with MVC/AngularMvcBundlingMinification/AngularMvcBundlingMinification/App_Start/BundleConfig.cs
+++ Bundling and Minifying Angular Scripts with MVC/AngularMvcBundlingMinification/AngularMvcBundlingMinification/App_Start/BundleConfig.cs
@@ -7,8 +7,9 @@ namespace AngularMvcBundlingMinification
         public static void RegisterScriptBundles(BundleCollection bundles)
         {
             const string ANGULAR_APP_ROOT = "~/Client/scripts/app/";
+            const string VIRTUAL_BUNDLE_PATH = ANGULAR_APP_ROOT + "main.js";
 
-            var scriptBundle = new ScriptBundle(ANGULAR_APP_ROOT + "main.js")
+            var scriptBundle = new ScriptBundle(VIRTUAL_BUNDLE_PATH)
                 .Include(ANGULAR_APP_ROOT + "app.js")
                 .IncludeDirectory(ANGULAR_APP_ROOT, "*.js", searchSubdirectories: true);
 
